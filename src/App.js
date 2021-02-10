f9cff2e7bdb53cb6f75b7db40b1cbf63ced9dd99
Emily Moore <emilymoore@users.noreply.github.com>
2021-02-10 14:30:55 -0600

    removed span left one div

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 
 import './App.css';
 import Weather from './Weather';
-import SearchForm from "./SearchForm";
+
                               
 
 export default function App() {
   return (
-    <div class="container">
-      <span class="weather-app-wrapper">
-        <span class="weather-app">
-          <SearchForm />
+      <div class="weather-app-wrapper">
+       
+
           <Weather />
 
           <p class="card-text refresh">
@@ -23,7 +22,7 @@ export default function App() {
             </button>
           </p>
          
-        </span>
+  
         <small class="source-code">
           <a
             href="https://github.com/emilydmoore/Weather-App"
@@ -34,7 +33,9 @@ export default function App() {
           </a>
           by Emily Moore
         </small>
-      </span>
+      
     </div>
   );
 }
+
+export default App;
