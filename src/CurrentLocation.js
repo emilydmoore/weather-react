2493ba1f56392096f82d8dd3737fbce5749569d8
Emily Moore <emilymoore@users.noreply.github.com>
2021-02-11 18:45:06 -0600

    removed Geocode npm & added Geolocation npm
    using API Open Weather Map data

--- src/CurrentLocation.js
+++ src/CurrentLocation.js
@@ -1,9 +1,13 @@
 import React from "react"; 
 import axios from "axios";
-
-
+import Geolocation from 'react-native-geolocation-service';
 
 export default function CurrentLocation(){
+    
+    Geolocation.ApiKey("30d908cd66a42b7d4c24ca6910b237cd");
+Geolocation.ApiUrl(`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${ApiKey}`);
+
+
 
 return (
 
